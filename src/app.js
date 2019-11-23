2b10872d77d25a1753a12e0919832fb04b93f463
JoseCoFR7 <00105018@uca.edu.sv>
2019-11-23 16:12:23 -0600

    [ParcialFinal] fix

--- src/app.js
+++ src/app.js
@@ -28,7 +28,9 @@ app.use('/', indexRoutes);
 //Iniciando servidor
 
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-});
 
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
